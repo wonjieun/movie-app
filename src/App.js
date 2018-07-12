710d8968bcb9a923d16b60168998c2a85f7c7c5d
wonjieun <wonjieunkk@gmail.com>
2018-07-12 17:49:14 +0900

    Props, Lists with .map, PropTypes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movies = [
+  {
+    title: "Avengers",
+    poster: "http://cdn.mydaily.co.kr/FILES/201804/201804060809134073_1.jpg"
+  },
+  {
+    title: "Harry Potter",
+    poster: "https://mvpo.us/img/P5159.jpg"
+  },
+  {
+    title: "Jurassic World",
+    poster: "https://www.bleedingcool.com/wp-content/uploads/2018/04/jurassic-world-poster.jpg"
+  },
+  {
+    title: "Spider-Man",
+    poster: "http://cdn.collider.com/wp-content/uploads/amazing-spider-man-movie-poster.jpg"
+  }
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
       </div>
     );
   }
