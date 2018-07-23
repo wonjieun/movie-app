08bd359057ae225ca8cf17af17f249612d620abc
wonjieun <wonjieunkk@gmail.com>
2018-07-23 20:56:35 +0900

    Loading States

--- src/App.js
+++ src/App.js
@@ -7,25 +7,6 @@ class App extends Component {
   // Update componentWillReceiveProps() -> shouldComponentUpdate() == true -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   state = {
-    greeting: 'Hello!',
-    movies: [
-      {
-        title: "Avengers",
-        poster: "http://www.futurerulerofmidgard.com/wp-content/uploads/2018/03/avengers-infinity-war-poster.jpg"
-      },
-      {
-        title: "Harry Potter",
-        poster: "https://mvpo.us/img/P5159.jpg"
-      },
-      {
-        title: "Jurassic World",
-        poster: "https://www.bleedingcool.com/wp-content/uploads/2018/04/jurassic-world-poster.jpg"
-      },
-      {
-        title: "Spider-Man",
-        poster: "http://cdn.collider.com/wp-content/uploads/amazing-spider-man-movie-poster.jpg"
-      }
-    ]
   }
 
   componentDidMount() {
@@ -34,26 +15,43 @@ class App extends Component {
       // console.log('hello')
       this.setState({
         movies: [
-          ...this.state.movies,  // leave what the array had before, add one more
-          // // replaced all movies with the last one
-         {
-           title: "Incredibles",
-           poster: "http://oyster.ignimgs.com/wordpress/stg.ign.com/2018/04/incredibles.jpg"
-         },
-
+          {
+            title: "Avengers",
+            poster: "http://www.futurerulerofmidgard.com/wp-content/uploads/2018/03/avengers-infinity-war-poster.jpg"
+          },
+          {
+            title: "Harry Potter",
+            poster: "https://mvpo.us/img/P5159.jpg"
+          },
+          {
+            title: "Jurassic World",
+            poster: "https://www.bleedingcool.com/wp-content/uploads/2018/04/jurassic-world-poster.jpg"
+          },
+          {
+            title: "Spider-Man",
+            poster: "http://cdn.collider.com/wp-content/uploads/amazing-spider-man-movie-poster.jpg"
+          },
+          {
+            title: "Incredibles",
+            poster: "http://oyster.ignimgs.com/wordpress/stg.ign.com/2018/04/incredibles.jpg"
+          }
         ]
       })
     }, 5000)
+  }
 
+  _renderMovies = () => {
+    // const movies = [<Movie props/>, <Movie props />]
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
