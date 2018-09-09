0efca22967399768c61150bebd82dd61d7ff5129
wonjieun <wonjieunkk@gmail.com>
2018-09-09 18:29:57 +0900

    #2 Await, Async

--- src/App.js
+++ src/App.js
@@ -9,19 +9,30 @@ class App extends Component {
   state = {}
 
   componentDidMount() {
-    // console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count'))
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
-    .then(potato => potato.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
     // const movies = [<Movie props/>, <Movie props />]
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      console.log(movie)
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
