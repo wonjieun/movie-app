6b79bf0a3b38ee71ad2437a546c82eeb4e8552d9
wonjieun <wonjieunkk@gmail.com>
2018-09-09 17:12:31 +0900

    Starting AJAX!

--- src/App.js
+++ src/App.js
@@ -6,38 +6,11 @@ class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update componentWillReceiveProps() -> shouldComponentUpdate() == true -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-  }
+  state = {}
 
   componentDidMount() {
-    // setTimeout(function() {  // old JavaScript
-    setTimeout(() => {
-      // console.log('hello')
-      this.setState({
-        movies: [
-          {
-            title: "Avengers",
-            poster: "http://www.futurerulerofmidgard.com/wp-content/uploads/2018/03/avengers-infinity-war-poster.jpg"
-          },
-          {
-            title: "Harry Potter",
-            poster: "https://mvpo.us/img/P5159.jpg"
-          },
-          {
-            title: "Jurassic World",
-            poster: "https://www.bleedingcool.com/wp-content/uploads/2018/04/jurassic-world-poster.jpg"
-          },
-          {
-            title: "Spider-Man",
-            poster: "http://cdn.collider.com/wp-content/uploads/amazing-spider-man-movie-poster.jpg"
-          },
-          {
-            title: "Incredibles",
-            poster: "http://oyster.ignimgs.com/wordpress/stg.ign.com/2018/04/incredibles.jpg"
-          }
-        ]
-      })
-    }, 5000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count'))
   }
 
   _renderMovies = () => {
